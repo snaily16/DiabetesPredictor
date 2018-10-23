533d5ce68d3c214c40c8303f4cc71e037afa0e84
snaily16 <snaily16@gmail.com>
2018-10-23 16:19:46 +0530

    Modifications

--- main.py
+++ main.py
@@ -3,33 +3,29 @@ import pickle
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-	
+	if request.method == 'POST':
+		height = float(request.form.get("height"))
+		weight = float(request.form.get('weight'))
+		bp = float(request.form.get('bp'))
+		glucose = float(request.form.get('glucose'))
+		insulin = float(request.form.get('insulin'))
+		age = float(request.form.get('age'))
+
+		bmi = weight/height**2
+		feature_array = [glucose, bp, insulin, bmi, age]
+		
+		model = pickle.load(open('model.pkl','rb'))
+		prediction = model.predict([feature_array])
+		if prediction == 0 :
+			ans = 'No diabetes'
+		else:
+			ans = 'Diabetes'
+		return render_template('homepage.html', answer=ans, bmi=bmi)
+
 	return render_template('homepage.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-	height = float(request.form.get("height"))
-	weight = float(request.form.get('weight'))
-	bp = float(request.form.get('bp'))
-	glucose = float(request.form.get('glucose'))
-	insulin = float(request.form.get('insulin'))
-	age = float(request.form.get('age'))
 
-	bmi = weight/height**2
-	feature_array = [glucose, bp, insulin, bmi, age]
-	
-	model = pickle.load(open('model.pkl','rb'))
-	prediction = model.predict([feature_array])
-	return render_template('result.html', prediction=prediction, bmi=bmi)
-
-"""
-@app.route('/result',methods=['POST','GET'])
-def result():
-	if request.method == 'POST':
-		result = request.form
-		return render_template('result.html', result=result)
-"""
 if __name__ == "__main__":
     app.run(debug=True)
